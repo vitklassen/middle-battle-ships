ef8fc7ad4cfa2496680cc687f0600a142ae149cd
Aleksei Usov <54838435+AlekseiUsov@users.noreply.github.com>
2025-10-04 11:26:52 +0300

    8.2. добавил ручку удаление, обновление и получения топика по id

--- packages/server/controllers/forum.ts
+++ packages/server/controllers/forum.ts
@@ -3,6 +3,7 @@ import { Comment, Topic } from '../models';
 
 const router = Router();
 
+// добавление
 router.post('/', async (req, res) => {
   const { title, content } = req.body;
   const ownerId = 1;
@@ -38,7 +39,113 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+// обновление
+router.patch('/:id', async (req, res) => {
+  const { id: topicId } = req.params;
+  const { title, content } = req.body;
+  const ownerId = 1;
+
+  if (typeof topicId !== 'number') {
+    res.status(400);
+    res.send({ status: 400, reason: 'Invalid topic ID' });
+    return;
+  }
+
+  if (!title && !content) {
+    res.status(400);
+    res.send({
+      status: 400,
+      reason: 'At least one of title or content must be provided',
+    });
+    return;
+  }
+
+  if (title && typeof title !== 'string') {
+    res.status(400);
+    res.send({ status: 400, reason: 'Invalid title param' });
+    return;
+  }
+
+  if (content && typeof content !== 'string') {
+    res.status(400);
+    res.send({ status: 400, reason: 'Invalid content param' });
+    return;
+  }
+
+  const topic = await Topic.findOne({
+    where: { id: topicId },
+  });
+
+  if (topic === null) {
+    res.status(404);
+    res.send({ status: 404, reason: 'Topic not found' });
+    return;
+  }
+
+  const updateData: { title?: string; content?: string } = {};
+
+  if (title) updateData.title = title;
+  if (content) updateData.content = content;
+
+  try {
+    await Topic.update(updateData, {
+      where: {
+        id: topicId,
+        owner_id: ownerId,
+      },
+    });
+
+    res.send('OK');
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Error) {
+      res.status(500).send({ reason: e.message });
+    } else {
+      res.status(500).send();
+    }
+  }
+});
+
+// удаление
+router.delete('/:id', async (req, res) => {
+  const { id: topicId } = req.params;
+
+  if (!topicId || typeof topicId !== 'number') {
+    res.status(400);
+    res.send({ status: 400, reason: 'Invalid topic ID' });
+    return;
+  }
+
+  try {
+    const topic = await Topic.findOne({
+      where: { id: topicId },
+    });
+
+    if (topic === null) {
+      res.status(404);
+      res.send({ status: 404, reason: 'Topic not found' });
+      return;
+    }
+
+    await Topic.destroy({
+      where: { id: topicId },
+    });
+
+    res.send('OK');
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Error) {
+      res.status(500).send({ reason: e.message });
+    } else {
+      res.status(500).send();
+    }
+  }
+});
+
+// добавление комментария
+router.post('/comment', async (req, res) => {
   const { topic_id: topicId, content } = req.body;
   const ownerId = 1;
 
@@ -84,11 +191,45 @@ router.post('/', async (req, res) => {
   }
 });
 
+// получение всех топиков
 router.get('/', async (_req, res) => {
   try {
     const topics = await Topic.findAll();
 
     res.send(topics);
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof Error) {
+      res.status(500).send({ reason: e.message });
+    } else {
+      res.status(500).send();
+    }
+  }
+});
+
+// получение топика по id
+router.get('/:id', async (req, res) => {
+  const { id: topicId } = req.params;
+
+  if (!topicId || typeof topicId !== 'number') {
+    res.status(400);
+    res.send({ status: 400, reason: 'Invalid topic ID' });
+    return;
+  }
+
+  try {
+    const topic = await Topic.findOne({
+      where: { id: topicId },
+    });
+
+    if (topic === null) {
+      res.status(404);
+      res.send({ status: 404, reason: 'Topic not found' });
+      return;
+    }
+
+    res.send(topic);
   } catch (e) {
     console.error(e);
 
